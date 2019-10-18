2d3424f406791c71d7a4ce0f64d7366b08e640c2
Jack Gugel <jagu2118@colorado.edu>
2019-10-18 04:51:49 +0000

    added main function to hash_functions.py

--- hash_functions.py
+++ hash_functions.py
@@ -17,5 +17,16 @@ def h_polynomial_rolling(key, N, p=53, m=2**64):
     return s % N 
 
 
+if __name__ == '__main__':    
+
+    for l in open(sys.argv[1]):
+        if sys.argv[2] == 'ascii':
+            print(h_ascii_sum(l, 1000))
+        elif sys.argv[2] == 'rolling':
+            print(h_polynomial_rolling(l, 1000))
+        elif sys.argv[2] == 'python':
+            print(h_python(l, 1000))
+
+
  
 
